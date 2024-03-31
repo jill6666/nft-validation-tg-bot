ed16cc37834ece5b97729705a4656ebf7d1ac67f
jill.chang <jillchang05@gmail.com>
2024-03-30 23:33:07 -0700

    feat: add welcome image

--- src/utils/onChooseWalletClick.ts
+++ src/utils/onChooseWalletClick.ts
@@ -1,6 +1,5 @@
 import { Context } from 'telegraf';
 import createDebug from 'debug';
-import adminWhitelist from './adminWhitelist';
 import { getWallets } from '../ton-connect/wallets';
 
 const debug = createDebug('bot:on_choose_wallet');
@@ -10,30 +9,29 @@ const onChooseWallet = async (ctx: Context) => {
     const userId = ctx.from?.id;
     if (!userId) throw new Error('User not found');
 
-    const isAdmin = await adminWhitelist(userId);
-    if (Boolean(isAdmin)) {
-      ctx.reply('You are already verified as Admin! 🎉');
-    } else {
-      const wallets = await getWallets();
-      const buttons = wallets.map((wallet) => ({
-        text: wallet.name,
-        callback_data: JSON.stringify({
-          method: 'select_wallet',
-          data: wallet.appName,
-        }),
-      }));
-      const length = buttons.length;
-      const cols = 2; // Number of columns
-      const rows = Math.ceil(length / cols); // Calculate the number of rows
-      const keyboard = [];
+    const wallets = await getWallets();
+    const buttons = wallets.map((wallet) => ({
+      text: wallet.name,
+      callback_data: JSON.stringify({
+        method: 'select_wallet',
+        data: wallet.appName,
+      }),
+    }));
+    const length = buttons.length;
+    const cols = 2; // Number of columns
+    const rows = Math.ceil(length / cols); // Calculate the number of rows
+    const keyboard = [];
 
-      for (let i = 0; i < rows; i++) {
-        const row = buttons.slice(i * cols, (i + 1) * cols);
-        keyboard.push(row);
-      }
-
-      await ctx.editMessageReplyMarkup({ inline_keyboard: keyboard });
+    for (let i = 0; i < rows; i++) {
+      const row = buttons.slice(i * cols, (i + 1) * cols);
+      keyboard.push(row);
     }
+
+    await ctx.editMessageMedia({
+      type: 'photo',
+      media: 'https://i.imgur.com/8nLFCVP.png',
+    });
+    await ctx.editMessageReplyMarkup({ inline_keyboard: keyboard });
   } catch (e) {
     console.log(e);
     debug(`Error in "onChooseWallet": ${JSON.stringify(e)}`);
