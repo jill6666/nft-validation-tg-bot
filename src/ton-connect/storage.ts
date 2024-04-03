1652b6f927d45c5696fcb83613390a96e9e41f4e
jill.chang <jillchang05@gmail.com>
2024-04-02 19:41:16 -0700

    feat: use kv instead storage

--- src/ton-connect/storage.ts
+++ src/ton-connect/storage.ts
@@ -1,24 +1,21 @@
 import { IStorage } from '@tonconnect/sdk';
-
-type TTempStorage = { [key: string]: any };
-let Storage: TTempStorage = {};
+import { kv } from '@vercel/kv';
 
 export class TonConnectStorage implements IStorage {
   constructor(private readonly chatId: number) {}
-
   private getKey(key: string): string {
     return this.chatId.toString() + key;
   }
 
   async removeItem(key: string): Promise<void> {
-    delete Storage[this.getKey(key)];
+    await kv.del(this.getKey(key));
   }
 
   async setItem(key: string, value: string): Promise<void> {
-    Storage[this.getKey(key)] = value;
+    await kv.set(this.getKey(key), value);
   }
 
   async getItem(key: string): Promise<string | null> {
-    return Storage[this.getKey(key)] || null;
+    return (await kv.get(this.getKey(key))) || null;
   }
 }
